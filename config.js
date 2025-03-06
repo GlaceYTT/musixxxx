7c88230bd412605faa182e4d46615872b3160add
GlaceYTT <official.glaceyt@gmail.com>
2025-03-06 19:41:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
      {
       name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      password: "xx123",
+      host: "78.46.65.243",
+      port:   3602,
       secure: false
     }
   ]
