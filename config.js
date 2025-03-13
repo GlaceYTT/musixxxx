e29a87ba807f2ad2ba8c8ab7fd95b10509528d6a
GlaceYTT <official.glaceyt@gmail.com>
2025-03-13 10:40:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "xx123",
-      host: "78.46.65.243",
-      port:   3602,
+     name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
       secure: false
     }
   ]
